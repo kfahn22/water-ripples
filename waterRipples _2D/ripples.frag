045ee2391f6bca0f75e7409d3965dbbbf3572936
Kathy <65121394+kfahn22@users.noreply.github.com>
2022-11-24 07:38:17 -0500

    Eliminating code that doesn't do anything

--- waterRipples _2D/ripples.frag
+++ waterRipples _2D/ripples.frag
@@ -7,15 +7,13 @@ varying vec2 vTexCoord;
 
 void main() {
   vec2 uv = vTexCoord.xy;
-  vec2 tx = vec2(1.0, 0.2);
-  vec2 ty = vec2(0.0, 1.0);
-  vec3 col = vec3(0.0);
+  vec2 tx = vec2(1.0, 0.0);
   
+  vec3 col; 
+
   float dx = (texture2D(uSampler, vTexCoord + tx).r - texture2D(uSampler, vTexCoord - tx).r) / 2.0;
-  float dy = (texture2D(uSampler, vTexCoord + ty).g - texture2D(uSampler, vTexCoord - ty).g) / 2.0;
   vec3 nx = clamp(vec3(tx, dx), vec3(0.0, 0.0), vec3(1.0, 1.0));
-  vec3 ny = vec3(ty, dy);
-  vec3 nmix = mix(nx, ny, 0.5);
-  col = nx;
+  col += nx;
+  
   gl_FragColor = vec4(col, 1.0);
 }
--- waterRipples _2D_color/sketch.js
+++ waterRipples _2D_color/sketch.js
@@ -1,6 +1,8 @@
 // Based on Water Ripples Coding Challenge by Daniel Shiffman
 // https://thecodingtrain.com/challenges/102-2d-water-ripple
 
+// https://thecodingtrain.com/challenges/90-dithering
+
 // https://codepen.io/Spongman/project/full/ArxVJQ/, which has a 3D version of water ripples, was also a resource.
 
 
@@ -14,40 +16,55 @@ var surfaceTexture;
 let current = new Array(W * H).fill(0);
 let previous = new Array(W * H).fill(0);
 
+let img;
+
 function preload() {
   surfaceShader = loadShader('ripples.frag', 'ripples.vert');
+  img = loadImage('assets/rocks.jpeg');
+}
+
+function imageIndex(img, x, y) {
+  return 4 * (x + y * img.width);
 }
 
 function setup() {
   // put setup code here
   createCanvas(800, 800, WEBGL);
   pixelDensity(1);
-  
+
   shader(surfaceShader);
   surfaceShader.setUniform('uTexSize', [W, H]);
   surfaceTexture = createGraphics(W, H);
-  surfaceTexture.background(0);
+  // surfaceTexture.background(0);
+  surfaceTexture.background(img);
   texture(surfaceTexture);
+
   noStroke();
 }
 
-function mouseDragged() {
-}
+function mouseDragged() {}
 
 function draw() {
+  let d = pixelDensity();
+  let x = floor(random(1, W - 1));
+  let y = floor(random(1, H - 1));
+  for (let i = 1; i < d; i++) {
+    for (let j = 1; j < d; j++) {
+      // loop over
+      previous[i + j * W] = img.get(i, j);
+      //previous[x + y * W] = get(x, y);
+      //previous[x + y * W] = random(10, 255);
+    }
+  }
 
-	let x = floor(random(1, W - 1));
-	let y = floor(random(1, H - 1));
-	previous[x + y * W] = random(10, 255);
-  
-	if (mouseIsPressed) {
-		const x = floor(map(mouseX, 0, width, 0, W));
-		const y = floor(map(mouseY, 0, height, 0, H));
-		previous[x + y * W] = 500;		
-	}
+  if (mouseIsPressed) {
+    const x = floor(map(mouseX, 0, width, 0, W));
+    const y = floor(map(mouseY, 0, height, 0, H));
+    previous[x + y * W] = 200;
+  }
 
   surfaceTexture.loadPixels();
- // for every non-edge element
+  // for every non-edge element
   for (let y = 1; y < H - 1; y++) {
     const yi = y * W;
     for (let x = 1; x < W - 1; x++) {
@@ -60,7 +77,6 @@ function draw() {
           previous[i + W]
         ) / 2 -
         current[i]);
-        constrain(val, 0, 255);
       current[i] = val;
       surfaceTexture.set(x, y, val);
     }
@@ -74,6 +90,6 @@ function draw() {
 
   // put drawing code here
   background(0);
-  
-  rect(-width/2, -height/2, width, height);
+
+  rect(-width / 2, -height / 2, width, height);
 }
